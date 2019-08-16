cc26281c6051848dea6e0edb976dc85838f981ec
Trang Le <trangle1405@gmail.com>
2019-08-16 17:46:02 +0700

    done

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 	$('[data-toggle="tooltip"]').tooltip(); 
 });
+
+// $(document).ready(function(){
+// $( window ).resize(function() {
+// if($(window).width()<769)
+//     {
+//         $("#changeClassNavbar").removeClass("navbar-expand-md");
+    
+//     }
+// });
+// });
